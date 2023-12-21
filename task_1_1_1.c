2f6bd3322a6b586eb2795193d190bbaf53ffb939
mariejonsson <jonsson139marie@gmail.com>
2023-12-21 21:14:54 +0100

    first script in c course 2020

---
+++ task_1_1_1.c
@@ -0,0 +1,9 @@
+#include <stdout>
+#include <stdin>
+#include <stdio>
+
+int main()
+{
+    printf("Lat oss gora lite matte");  /* how to comment multiple lines */
+    return 0;                           // main function returns status code '0' and ends program
+}
