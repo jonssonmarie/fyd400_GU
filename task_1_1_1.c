1927cd496ec4d474c56fbd7eb478ff7e41886692
Marie Jönsson <89392551+jonssonmarie@users.noreply.github.com>
2023-12-22 16:48:08 +0100

    Delete task_1_1_1.c
    unnecessary, correct file are added now

--- task_1_1_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdout>
-#include <stdin>
-#include <stdio>
-
-int main()
-{
-    printf("Lat oss gora lite matte");  /* how to comment multiple lines */
-    return 0;                           // main function returns status code '0' and ends program
-}
